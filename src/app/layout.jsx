14dfc2b19f8c7483e6ee562b0feddf49845b0abe
MuhammadAli <MuhammadAli>
2025-12-17 21:29:44 +0500

    add style

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,14 +1,36 @@
+import { Roboto, Roboto_Condensed } from "next/font/google";
 import "./globals.css";
 
+const roboto = Roboto({
+  weight: ["400", "500", "700"],
+  subsets: ["latin", "cyrillic"],
+  variable: "--text-font",
+  display: "swap",
+});
+
+const robotoCondensed = Roboto_Condensed({
+  weight: ["700"],
+  subsets: ["latin", "cyrillic"],
+  variable: "--heading-font",
+  display: "swap",
+});
+
 export const metadata = {
-  title: "Akademnashr",
-  description: "Akademnashr promo sayt",
+  title: "Akademnashr - Ozbek tilidagi kitoblar",
+  description:
+    "Dasturlash asoslariga oid ozbek tilidagi ilk eng mukammal qollanma",
+  keywords: "kitoblar, dasturlash, python, ozbek tilida",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="uz">
-      <body>{children}</body>
+      <body className={`${roboto.variable} ${robotoCondensed.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
